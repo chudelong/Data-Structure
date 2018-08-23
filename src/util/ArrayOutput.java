5a798a15e1342df9f8878511f0df2bfcc300c9ca
chudelong <1114553919@qq.com>
2018-08-23 15:32:39 +0800

    -接上一步提交内容

---
+++ src/util/ArrayOutput.java
@@ -0,0 +1,13 @@
+package util;
+
+public class ArrayOutput {
+	/**
+	 * ±éÀúÊı×é
+	 * @param arr
+	 */
+	public static void arraySort(int[] arr) {
+		for(int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + ",");
+		}
+	}
+}
